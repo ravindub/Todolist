bc4860800801ccc3146315c643a7cb9d84d9811d
ravindub <ravindubandara075@gmail.com>
2023-09-04 01:00:17 +0300

    complete app

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
+const items = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("hello");
+  let day = date.getDate();
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let itemName = req.body.newItem;
+
+  items.push(itemName);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
